535c241a6ecf7b6d8f01595bd167a22671a9172c
xkdax <kdanew@yandex.ru>
2021-05-24 22:42:10 +0300

    dobavil huiny

--- main.py
+++ main.py
@@ -125,4 +125,22 @@ inp = input("Please enter the number of the habitat you would like to view: > ")
 while inp != "exit":
    print(animals[int(inp) - 1])
    inp = input("Please enter the number of the habitat you would like to view: > ")
-print("See you later!")   
+print("See you later!")
+
+
+
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+#def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+#    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+
+# Press the green button in the gutter to run the script.
+#if __name__ == '__main__':
+#    print_hi('PyCharm')
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
